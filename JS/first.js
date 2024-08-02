bf0e0bacad8588633028dbbd8c0e281ee3d8b8ec
KOMAL22-CODER <kvermar22@gmail.com>
2024-08-02 23:04:42 +0530

    data files

---
+++ JS/first.js
@@ -0,0 +1,31 @@
+let a=10;
+b=20;
+console.log(a+b)
+a=30;
+b=20;
+console.log(a+b)
+
+let x=10;
+const y=20;
+console.log(x+y)
+
+let Rahul=20;
+const rahul=30;
+console.log(Rahul)
+console.log(rahul)
+
+let sur4aj=5;
+const avi7raj=50;
+console.log(avi7raj)
+
+let meg_ha=60;
+console.log(meg_ha)
+
+
+
+
+
+
+
+
+ 
---
+++ JS/operators.js
@@ -0,0 +1,31 @@
+// arithmetic operators
+
+let a=10;
+b=20;
+console.log(a+b)
+
+let x=10;
+y=20;
+console.log(x-y)
+
+let u=10;
+v=20;
+console.log(u*v)
+
+let t=10;
+z=20;
+console.log(t/z)
+
+let i=10;
+l=20;
+console.log(i%l)
+
+//output with message
+let c=10;
+b=20;
+console.log("the sum of a and b is:",c+b)
+
+a++;
+console.log(a);
+
+
