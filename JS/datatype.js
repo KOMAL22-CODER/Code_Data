4efb2119a7c72d6c2616bc36161e895109d1f727
KOMAL22-CODER <kvermar22@gmail.com>
2024-08-03 15:40:07 +0530

    data type checking

---
+++ JS/datatype.js
@@ -0,0 +1,29 @@
+let c=150;
+let d=200;
+console.log(typeof(c));
+
+let s=15.87;
+let x=20.54;
+console.log(typeof(s));
+
+let n="Ishant";
+console.log(typeof(n));
+
+let g=true;
+console.log(typeof(g));
+
+let h=null;
+console.log(typeof(h));
+
+let j=undefined;
+console.log(typeof(j));
+
+// type of operator
+//string concatination
+a="Mohit";
+b="Raksha";
+console.log(a+b);
+
+
+
+
