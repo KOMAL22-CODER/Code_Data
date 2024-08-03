e05a9599c1682cca36b5911f9789f28194e24728
KOMAL22-CODER <kvermar22@gmail.com>
2024-08-03 15:35:36 +0530

    logical opertors

---
+++ JS/logical.js
@@ -0,0 +1,23 @@
+let a=50;
+let b=80;
+let c=58;
+console.log(a<b && b<c);
+
+console.log(a<b || b<c);
+
+console.log(a<b || b>c);
+
+console.log(a<b && b>c);
+
+console.log(!(a<b && b>c));
+
+
+
+
+
+
+
+
+
+
+
