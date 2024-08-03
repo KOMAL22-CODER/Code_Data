b139f366295d930fc8bac75b7182866fd5cdf888
KOMAL22-CODER <kvermar22@gmail.com>
2024-08-03 15:25:03 +0530

    relaional operators

---
+++ JS/relational.js
@@ -0,0 +1,23 @@
+//relational operator <,>,<=,>=,==,===,!==,!=
+let a=50;
+let b=100;
+console.log(a>b)
+
+console.log(a<b)
+
+console.log(a==b);
+
+console.log(a>=b);
+
+console.log(a<=b);
+
+console.log(a!=b);
+
+let shivam=40;
+let dhruv=60;
+console.log(shivam==dhruv);
+
+// === will also check the type of data also
+console.log(shivam===dhruv);
+
+
